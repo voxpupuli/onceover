51baf1e1fe4ef651eb5f0d717de2d5be8d7c44d3
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-09-28 12:02:22 +1000

    Allowed vagrant boxes generation to accept arrays of facts

--- lib/controlrepo/beaker.rb
+++ lib/controlrepo/beaker.rb
@@ -1,16 +1,44 @@
-class Controlrepo::Beaker
+class Controlrepo_beaker
   def self.facts_to_vagrant_box(facts)
-    # Gets the most similar vagrant box to the facts set provided
-    if facts['os']['distro']['id'] == 'Ubuntu'
-      os = 'ubuntu'
-      version = facts['os']['distro']['release']['major']
-    elsif facts['os']['distro']['id'] == 'Debian'
-      os = 'Debian'
-      version = facts['os']['distro']['release']['major']
-    elsif facts['os']['family'] == "RedHat"
-      os = 'centos'
-      version = "#{facts['os']['release']['major']}.#{facts['os']['release']['minor']}"
+    # Gets the most similar vagrant box to the facts set provided, will accept a single fact
+    # se or an array
+
+    if facts.is_a?(Array)
+      returnval = []
+      facts.each do |fact|
+        returnval << self.facts_to_vagrant_box(fact)
+      end
+      return returnval
     end
+
+    begin
+      if facts['os']['distro']['id'] == 'Ubuntu'
+        os = 'ubuntu'
+        version = facts['os']['distro']['release']['major']
+      end
+    rescue
+      # Do nothing, this is the easiest way to handle the hash bing in different formats
+    end
+
+    begin
+      if facts['os']['distro']['id'] == 'Debian'
+        os = 'Debian'
+        version = facts['os']['distro']['release']['major']
+      end
+    rescue
+      # Do nothing
+    end
+
+    begin
+      if facts['os']['family'] == "RedHat"
+        os = 'centos'
+        version = "#{facts['os']['release']['major']}.#{facts['os']['release']['minor']}"
+      end
+    rescue
+      # Do nothing
+    end
+
+    raise "Could not detect the os when generating vagrant box name" unless os.is_a?(String)
 
     if facts['os']['architecture'] =~ /64/
       arch = '64'
