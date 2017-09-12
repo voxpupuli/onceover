4b673b9b68325afaa442e300caa54da7bc3cf891
Marek Maksimczyk <marek.maksimczyk@xstream.net>
2017-09-12 16:07:59 +0200

    Add acceptance tests for simple init scenario
    Create cucumber's init feature which covers two basic scenarios (correct
    and incorrect control repo). To do so I added control repo based on
    puppet labs' one (https://github.com/puppetlabs/control-repo).
    Additional I refactor cucumber's step definitions and added Command
    helper class.

--- features/step_definitions/common.rb
+++ features/step_definitions/common.rb
@@ -1,18 +1,60 @@
-Given /^onceover executable$/  do
-  @executable = ENV["BUNDLE_GEMFILE"] ? "bundle exec onceover " : "onceover "
+Given /^onceover executable$/ do
+  @cmd = Command.new
 end
 
-When /^I run onceover command "([^"]*)"$/  do |arg1|
-  command = @executable + arg1
-  puts command
-  @output = `#{command}`
-  expect($?.success?).to be true
+Given(/^control repo "([^"]*)"$/) do |controlrepo_name|
+  @cmd.controlrepo = controlrepo_name
+  FileUtils.rm_rf "tmp/tests/#{controlrepo_name}"
+  FileUtils.mkdir_p 'tmp/tests'
+  FileUtils.cp_r "spec/fixtures/#{controlrepo_name}", 'tmp/tests'
+end
+
+Given(/^control repo "([^"]*)" without "([^"]*)"$/) do |controlrepo_name, filename|
+  step %Q(control repo "#{controlrepo_name}")
+  controlrepo_path = "tmp/tests/#{controlrepo_name}"
+  FileUtils.rm_rf( controlrepo_path + "/#{filename}" )
+end
+
+Then(/^I should see all tests pass$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /^I run onceover command "([^"]*)"$/  do |command|
+  @cmd.command = command
+  puts @cmd
+  @cmd.run
 end
 
 Then /^I see help for commands: "([^"]*)"$/ do |commands|
-  commands_help = @output[/COMMANDS(.*)OPTIONS/m, 1]
+  # Get chunk of output between COMMANDS and OPTION, there should be help section
+  commands_help = @cmd.output[/COMMANDS(.*)OPTIONS/m, 1]
   commands.split(',').each do |command|
     result = commands_help.match(/^\s+#{command.strip}.+\n/)
     puts result.to_s if expect(result).not_to be nil
+  end
+end
+
+Then(/^I should not see any errors$/) do
+  expect(@cmd.success?).to be true
+end
+
+Then(/^I should see error with message pattern "([^"]*)"$/) do |err_msg_regexp|
+  expect(@cmd.success?).to be false
+  puts @cmd.output
+  expect(@cmd.output.match err_msg_regexp).to_not be nil
+end
+
+Then(/^I should see generated all necessary files and folders$/) do
+  controlrepo_path = "tmp/tests/controlrepo_basic/"
+  files = [ 'spec/onceover.yaml', 'Rakefile', 'Gemfile' ].map { |x| controlrepo_path + x }
+  folders = [ 'spec/factsets', 'spec/pre_conditions'].map! { |x| controlrepo_path + x}
+
+  files.each do |file|
+    puts file
+    expect( File.exist? file ).to be true
+  end
+  folders.each do |folder|
+    puts folder
+    expect( Dir.exist? folder ).to be true
   end
 end
