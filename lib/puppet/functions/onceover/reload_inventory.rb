01d909f5482a41e86b58e03b9f645838a4555e0d
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2019-08-29 13:47:45 +0100

    Finished fuill accepotance tests

--- lib/puppet/functions/onceover/reload_inventory.rb
+++ lib/puppet/functions/onceover/reload_inventory.rb
@@ -24,10 +24,19 @@ Puppet::Functions.create_function(:'onceover::reload_inventory') do
     # Re-initialise a new object
     new_inventory  = Bolt::Inventory.new(YAML.safe_load(File.read(inventory_file)))
 
+    instance_vars_exclude = [
+      :@target_vars,
+      :@target_facts,
+      :@target_features,
+    ]
+
     # Go through and replace all of the instance variables because this seems
     # to be the only way to get this to work
     old_inventory.instance_variables.each do |var|
-      old_inventory.instance_variable_set(var, new_inventory.instance_variable_get(var))
+      # Replace instance variables unless thay need to be kept
+      unless instance_vars_exclude.include? var
+        old_inventory.instance_variable_set(var, new_inventory.instance_variable_get(var))
+      end
     end
   end
 end
