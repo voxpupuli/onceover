195b35130435cf3e47efd672e54ef0df5be3db93
Gavin Riggi <108762141+griggi-ws@users.noreply.github.com>
2026-02-13 13:44:20 -0500

    fix: git tree url for openvox-agent in vendored_modules

--- lib/onceover/vendored_modules.rb
+++ lib/onceover/vendored_modules.rb
@@ -49,7 +49,7 @@ class Onceover
       # Get the entire file tree of the puppetlabs/puppet-agent repository
       # https://docs.github.com/en/rest/git/trees?apiVersion=2022-11-28#get-a-tree
       puppet_agent_tree = query_or_cache(
-        "https://api.github.com/repos/OpenVoxProject/openvox-agent/git/trees/#{@puppet_version}",
+        "https://api.github.com/repos/OpenVoxProject/openvox/git/trees/#{@puppet_version}",
         { recursive: true },
         component_cache('repo_tree'),
       )
